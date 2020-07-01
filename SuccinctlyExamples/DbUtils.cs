25073a473055738d86ec682ae7846896b68c1203
johphil <johphilencarnacion@gmail.com>
2020-07-02 01:45:16 +0800

    Insert, Update, Search and Delete with SQL Server

---
+++ SuccinctlyExamples/DbUtils.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SuccinctlyExamples
+{
+    public static class DbUtils
+    {
+        public static object ToDbParameter<T>(this T? value)
+        where T : struct
+        {
+            object dbValue = value;
+            if (dbValue == null)
+            {
+                dbValue = DBNull.Value;
+            }
+            return dbValue;
+        }
+        public static object ToDbParameter(this object value)
+        {
+            object dbValue = value;
+            if (dbValue == null)
+            {
+                dbValue = DBNull.Value;
+            }
+            return dbValue;
+        }
+    }
+}
---
+++ SuccinctlyExamples/Person.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SuccinctlyExamples
+{
+    class Person
+    {
+        public int ID { get; set; }
+        public string FirstName { get; set; }
+        public string MiddleName { get; set; }
+        public string LastName { get; set; }
+        public DateTime? DateOfBirth { get; set; }
+        public int? GenderId { get; set; }
+        public string Gender { get; set; }
+    }
+}
